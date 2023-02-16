f83eb57d65b2d8402313459143036ca98a8d09c0
David Lavieri <daviddlavier@gmail.com>
2023-02-16 15:26:08 +0100

    patch dist

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+const $cf838c15c8b009ba$var$initialOptions = {
+    selector: ".hc-themes-widget"
+};
+const $cf838c15c8b009ba$var$ThemesWidget = (options)=>{
+    options = {
+        ...$cf838c15c8b009ba$var$initialOptions,
+        ...options
+    };
+};
+var $cf838c15c8b009ba$export$2e2bcd8739ae039 = $cf838c15c8b009ba$var$ThemesWidget;
+
+
+export {$cf838c15c8b009ba$export$2e2bcd8739ae039 as default};
+//# sourceMappingURL=main.js.map
